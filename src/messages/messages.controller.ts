24ff247b600d298fd076385334e4c22e290f04bf
deepak-delta <ddk3857@gmail.com>
2022-08-09 13:09:27 +0530

    Messages Controller

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('/messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'This action returns all messages';
+  }
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
+}
