9921492a7d3b23ea98bcf1801234a94c1f260b2b
deepak-delta <ddk3857@gmail.com>
2022-08-13 20:28:48 +0530

    Built-In ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe()); //For request validation globally
   await app.listen(3000);
 }
 bootstrap();
