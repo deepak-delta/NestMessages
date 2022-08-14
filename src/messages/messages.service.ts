06e061fa4376e43b7e1cea85880c9bc9b0c28e87
deepak-delta <ddk3857@gmail.com>
2022-08-14 20:02:34 +0530

    Dependency Injection

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,10 +1,10 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepository: MessagesRepository;
-  constructor() {
-    this.messagesRepository = new MessagesRepository();
-  }
+  constructor(public messagesRepository: MessagesRepository) {}
+
   findOne(id: string) {
     return this.messagesRepository.findOne(id);
   }
